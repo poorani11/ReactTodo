2271f85603c5415c978bd7f590db927b3e59f400
poorani11 <poorani.mgr@gmail.com>
2018-06-13 04:13:06 +0530

    Added search components

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -47,12 +47,13 @@ var TodoApp = React.createClass({
         })
     },
     render: function() {
-        var {todos} = this.state;
+        var {todos, showCompleted, searchText} = this.state;
+        var filteredTodos = TodoAPI.filterTodos(todos, showCompleted, searchText);
         return (
             <div className="row">
                <div className="column small-centered medium-6 large-4">
                     <TodoSearch onSearch={this.handleSearch} />
-                    <TodoList todos={todos} onToggle={this.handleToggle}/>
+                    <TodoList todos={filteredTodos} onToggle={this.handleToggle}/>
                     <AddTodo onAddTodo={this.handleAddTodo}/>
                 </div>
             </div>
