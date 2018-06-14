15af02d980580ecd42f3515c0768070f81990636
poorani11 <poorani.mgr@gmail.com>
2018-06-14 21:25:12 +0530

    modified todosearch

--- app/app.jsx
+++ app/app.jsx
@@ -11,10 +11,6 @@ store.subscribe(() => {
     console.log('New State', store.getState());
 });
 
-store.dispatch(actions.addTodo('Clean the Yard'));
-store.dispatch(actions.setSearchText('yard'));
-store.dispatch(actions.toggleShowCompleted());
-
 // Load foundation
 $(document).foundation();
 
